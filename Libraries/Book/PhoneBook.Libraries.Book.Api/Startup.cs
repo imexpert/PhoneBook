e4f497b316f8d2de0d843929a49956a1a534a69c
ali.unal <aosmanunal@gmail.com>
2021-10-30 13:27:04 +0300

    book.api projesinin configurasyon ayarları yapıldı

--- Libraries/Book/PhoneBook.Libraries.Book.Api/Startup.cs
+++ Libraries/Book/PhoneBook.Libraries.Book.Api/Startup.cs
@@ -1,40 +1,86 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PhoneBook.Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using PhoneBook.Core.Extensions;
+using PhoneBook.Core.Utilities.IoC;
+using PhoneBook.Libraries.Book.Business;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PhoneBook.Libraries.Book.Api
 {
-    public class Startup
+    public class Startup : BusinessStartup
     {
+        /// <summary>
+        /// Constructor of <c>Startup</c>
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="hostEnvironment"></param>
+        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
+            : base(configuration, hostEnvironment)
+        {
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public override void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "AllowOrigin",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml"));
+            });
+
+            services.AddTransient<PostgreSqlLogger>();
+
+            base.ConfigureServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            ServiceTool.ServiceProvider = app.ApplicationServices;
+
+            app.UseDeveloperExceptionPage();
+
+            app.ConfigureCustomExceptionMiddleware();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("v1/swagger.json", "Phone Book Api");
+                c.DocExpansion(DocExpansion.None);
+            });
+
+            app.UseCors("AllowOrigin");
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
---
+++ Libraries/Book/PhoneBook.Libraries.Book.Business/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Autofac;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PhoneBook.Core.DependencyResolvers;
+using PhoneBook.Core.Extensions;
+using PhoneBook.Core.Utilities.IoC;
+using PhoneBook.Core.Utilities.MessageBrokers.RabbitMq;
+using PhoneBook.Libraries.Book.Business.DependencyResolvers;
+using PhoneBook.Libraries.Book.DataAccess.Concrete.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Libraries.Book.Business
+{
+    public partial class BusinessStartup
+    {
+        public IConfiguration Configuration { get; }
+
+        protected IHostEnvironment HostEnvironment { get; }
+
+        public BusinessStartup(IConfiguration configuration, IHostEnvironment hostEnvironment)
+        {
+            Configuration = configuration;
+            HostEnvironment = hostEnvironment;
+        }
+
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            var coreModule = new CoreModule();
+
+            services.AddDependencyResolvers(Configuration, new ICoreModule[] { coreModule });
+
+            services.AddSingleton<ConfigurationManager>();
+
+            services.AddTransient<IMessageBrokerHelper, MqQueueHelper>();
+            services.AddTransient<IMessageConsumer, MqConsumerHelper>();
+
+            services.AddAutoMapper(typeof(ConfigurationManager));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(typeof(BusinessStartup).GetTypeInfo().Assembly);
+
+            ValidatorOptions.Global.DisplayNameResolver = (type, memberInfo, expression) =>
+            {
+                return memberInfo.GetCustomAttribute<DisplayAttribute>()
+                    ?.GetName();
+            };
+
+            services.AddDbContext<ProjectDbContext>();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="builder"></param>
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new AutofacBusinessModule(new ConfigurationManager(Configuration, HostEnvironment)));
+        }
+    }
+}

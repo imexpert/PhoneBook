81de4153e4f54bd5d96fa884a5ed9ea452d2583b
ali.unal <aosmanunal@gmail.com>
2021-10-30 17:23:51 +0300

    ui projesi ekranları hazırlanıyor.

---
+++ Presentation/PhoneBook.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Web.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPersonService _personService;
+
+        public HomeController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
